9862ca3ad09cd1c7c1f80e17c4ac70cea52e0a6b
Yehiyam Livneh <yehiyam@gmail.com>
2020-07-23 15:55:49 +0300

    initial

---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { promisify } = require('util');
+
+const getPrNumber = () => {
+    const pullRequest = github.context.payload.pull_request;
+    if (!pullRequest) {
+        return undefined;
+    }
+    return pullRequest.number;
+}
+
+const getChangedServices = async (client, prNumber) => {
+    const listFilesResponse = await client.pulls.listFiles({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: prNumber
+    });
+
+    const changedFiles = listFilesResponse.data.map(f => f.filename);
+
+    core.debug('found changed files:');
+    const changedServices = new Set();
+    for (const file of changedFiles) {
+        core.debug('  ' + file);
+    }
+
+    return changedFiles;
+}
+
+// most @actions toolkit packages have async methods
+async function run() {
+    try {
+        const token = core.getInput('repo-token', { required: true });
+        const client = new github.GitHub(token);
+        const prNumber = getPrNumber();
+        if (!prNumber) {
+            throw new Error('Could not get pull request number from context, exiting');
+        }
+        core.debug(`fetching changed services for pr #${prNumber}`);
+        const changed = await getChangedServices(client, prNumber);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
